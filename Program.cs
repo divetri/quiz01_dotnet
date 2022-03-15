b6a20f14d607810ce1d50a00f0b13ad3233b4f99
Divetri Ayu Rahmawati <54519415+divetri@users.noreply.github.com>
2022-03-15 17:12:18 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Quiz01.Solution.Soal1();
+Console.ReadLine();
---
+++ Solution.cs
@@ -0,0 +1,275 @@
+﻿namespace Quiz01
+{
+    public class Solution
+    {
+        public static void Soal1()
+        {
+            Random RandomClass = new Random();
+            bool correct = false;
+            int x = RandomClass.Next(1, 10);
+            Console.WriteLine("Guess magic number between 1-20");
+            while (!correct)
+            {
+                Console.Write("Enter your guess: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == x)
+                {
+                    Console.WriteLine("Well done! The answer was " + x + "Lanjut?");
+                    string next = Console.ReadLine();
+                    if (next == "y" | next == "Y")
+                    {
+                        Soal1();
+                    }
+                    else if (next == "n" | next == "N")
+                    {
+                        Console.WriteLine("You are quit.");
+                        break;
+                    }
+                }
+                else if (guess != x)
+                {
+                    if (guess < x)
+                    {
+                        Console.WriteLine("Your guess is too low");
+                    }
+                    else if (guess > x)
+                    {
+                        Console.WriteLine("Your guess is too high");
+                    }
+                }
+            }
+        }
+        public static void Soal2()
+        {
+            List<int> list = new List<int>();
+            bool correct = false;
+            while (!correct)
+            {
+                Console.Write("Enter integer [type '0' for exit]: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number != 0)
+                {
+                    list.Add(number);
+                }
+                else
+                {
+                    int[] terms = list.ToArray();
+                    Console.WriteLine("Angka Terbesar: " + terms.Max());
+                }
+            }
+        }
+        public static void Soal3()
+        {
+            Console.Write("Enter numbers: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number / 1 > 10)
+            {
+                if (number / 10 > 10)
+                {
+                    if (number / 100 > 10)
+                    {
+                        if (number / 1000 > 10)
+                        {
+                            if (number / 10000 > 10)
+                            {
+                                if (number / 100000 > 10)
+                                {
+                                    if (number / 1000000 > 10)
+                                    {
+                                        if (number / 10000000 > 10)
+                                        {
+                                            if (number / 100000000 > 10)
+                                            {
+                                                if (number / 1000000000 > 10)
+                                                {
+                                                    Console.WriteLine("Total digit : 11");
+                                                }
+                                                else
+                                                {
+                                                    Console.WriteLine("Total digit : 10");
+                                                }
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("Total digit : 9");
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Total digit : 8");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Total digit : 7");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Total digit : 6");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Total digit : 5");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Total digit : 4");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Total digit : 3");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Total digit : 2");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Total digit : 1");
+            }
+        }
+        public static void Soal4()
+        {
+            Console.Write("Enter numbers: ");
+            string n = Console.ReadLine();
+
+            for (int i = n.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(n[i]);
+            }
+
+        }
+        public static void Soal5()
+        {
+            Console.Write("Enter numbers: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+        public static void Soal6()
+        {
+            Console.Write("Enter numbers: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                int faktor = 0;
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        faktor++;
+                        break;
+                    }
+                }
+                if (faktor == 0 && i > 1)
+                    Console.Write(i + " ");
+            }
+        }
+        public static void Soal7()
+        {
+            int p = 0;
+            Console.Write("Enter strings: ");
+            string x = Console.ReadLine();
+            string n = x.ToLower();
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == n[n.Length - 1 - i])
+                {
+                    p += 1;
+                }
+
+            }
+            if (p == n.Length)
+            {
+                Console.Write("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+        public static void Soal8()
+        {
+            Console.Write("Enter strings: ");
+            string x = Console.ReadLine();
+            string n = x.ToLower();
+            int v = 0;
+            int c = 0;
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == 'a' || n[i] == 'i' || n[i] == 'u' || n[i] == 'e' || n[i] == 'o')
+                {
+                    v++;
+                }
+                else
+                {
+                    c++;
+                }
+            }
+            Console.WriteLine($"total vowel {v}");
+            Console.WriteLine($"total consonant {c}");
+        }
+        public static void Soal9()
+        {
+            int s = 1;
+            Console.Write("Enter strings: ");
+            string n = Console.ReadLine();
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == ' ')
+                {
+                    s++;
+                }
+            }
+            Console.WriteLine($"total words {s}");
+        }
+        public static void Soal11()
+        {
+            DateTime today = DateTime.Today;
+            Console.Write("Hari "+today.DayOfWeek+": ");
+            Console.WriteLine(today.ToShortDateString());
+            int jumat = ((int)DayOfWeek.Friday - (int)today.DayOfWeek + 7) % 7;
+            Console.WriteLine(jumat+" hari lagi!");
+        }
+        public static void Soal12()
+        {
+            int i = 0;
+            bool correct = false;
+            Console.Write("Enter strings: ");
+            string n = Console.ReadLine();
+            string text = n;
+            Console.Write("Find strings: ");
+            string x = Console.ReadLine();
+            while (!correct)
+            {
+                if (n.IndexOf(x) >= 0)
+                {
+                    i++;
+                    int nextIndex = n.IndexOf(x) + x.Length;
+                    n = n.Substring(nextIndex);
+                }
+                else
+                {
+                    correct = true;
+                    Console.WriteLine("Total kata yang muncul: "+i+" Dari text: "+text);
+                }
+            }
+        }
+    }
+}
